50aef7ce0472db674722291129d46758ef864872
rezonmain <adiazgomez101@gmail.com>
2022-08-13 16:58:26 -0700

    initialized nextjs project for front end server explorer

---
+++ web/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return (
+		<div className='bg-neutral-800'>
+			<h1 className='text-2xl text-white'>Hello mc</h1>
+		</div>
+	);
+};
+
+export default Home;
