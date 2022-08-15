ad9914aa1a835af1a69fb2d5cb69b8ab93e966de
rezonmain <adiazgomez101@gmail.com>
2022-08-14 23:48:23 -0700

    added infinite scrolling

--- web/components/ServerList/ServerList.tsx
+++ web/components/ServerList/ServerList.tsx
@@ -1,57 +1,44 @@
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 import PropertyParser from '../../lib/classes/PropertyParser';
 import { trpc } from '../../utils/trpc';
 import ServerCard from '../ServerCard/ServerCard';
 import ServerCardSkeleton from '../ServerCardSkeleton/ServerCardSkeleton';
 
 const ServerList = () => {
+	// FIX: Doesn't show every entry, probable something to do with cursor in the query definition
 	const limit = 5;
-	const {
-		data,
-		error,
-		isLoading,
-		fetchNextPage,
-		hasNextPage,
-		isFetching,
-		isFetchingNextPage,
-	} = trpc.useInfiniteQuery(['mostRecent', { limit }], {
-		getNextPageParam: (lastPage) => lastPage.nextCursor,
-	});
+	const { data, isLoading, fetchNextPage, hasNextPage } = trpc.useInfiniteQuery(
+		['mostRecent', { limit }],
+		{
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+		}
+	);
 
 	if (isLoading) {
 		return <ServerCardSkeleton amount={limit} />;
 	}
 	return (
-		<>
-			<ul>
-				{data?.pages.map((group, i) => (
-					<React.Fragment key={i}>
-						{group.items.map((server) => {
-							const p = new PropertyParser(server);
-							const parsedServer = p.getParsedServer();
-							return <ServerCard {...parsedServer} />;
-						})}
-					</React.Fragment>
-				))}
-			</ul>
+		<InfiniteScroll
+			loadMore={() => fetchNextPage()}
+			hasMore={hasNextPage}
+			loader={<ServerCardSkeleton amount={limit} />}
+		>
 			<div>
-				<button
-					onClick={() => fetchNextPage()}
-					disabled={!hasNextPage || isFetching}
-				>
-					{isFetchingNextPage
-						? 'Loading more..'
-						: hasNextPage
-						? 'Load More'
-						: 'Nothing more to load'}
-				</button>
-				<div>
-					{isFetching && !isFetchingNextPage ? (
-						<ServerCardSkeleton amount={limit} />
-					) : null}
-				</div>
+				<ul>
+					{data?.pages.map((group, i) => (
+						<React.Fragment key={i}>
+							{group.items.map((server) => {
+								const p = new PropertyParser(server);
+								const parsedServer = p.getParsedServer();
+								return <ServerCard {...parsedServer} />;
+							})}
+						</React.Fragment>
+					))}
+				</ul>
+				{!isLoading && !hasNextPage && <div>Nothing more to show</div>}
 			</div>
-		</>
+		</InfiniteScroll>
 	);
 };
 export default ServerList;
