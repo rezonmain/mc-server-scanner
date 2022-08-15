b7e712bf717081840e61d65142302195dee859d8
rezonmain <adiazgomez101@gmail.com>
2022-08-15 01:25:28 -0700

    debugging prod

--- web/db/Db.ts
+++ web/db/Db.ts
@@ -4,11 +4,17 @@ class DB {
 	/* In development use the test database
 	in production use the scanner_db database */
 	static URI =
-		process.env.NODE_ENV === 'development'
+		process.env.NODE_ENV === 'production'
+			? (process.env.MONGO_URI as string)
+			: process.env.NODE_ENV === 'development'
 			? (process.env.MONGO_URI_TEST as string)
-			: (process.env.MONGO_URI as string);
+			: '';
 
-	constructor() {}
+	constructor() {
+		if (DB.URI !== '') {
+			console.log('got it');
+		}
+	}
 
 	connect = async () => {
 		mongoose.connect(DB.URI);
