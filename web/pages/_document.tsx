8256f7d33a824d217aa8f3014b5c4d4f4a5be83c
rezonmain <adiazgomez101@gmail.com>
2022-08-20 16:56:14 -0700

    added favicon

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				<link rel='shortcut icon' href='/images/favicon.ico' />
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' />
 				<link
