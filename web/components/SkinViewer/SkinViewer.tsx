695a442b965c47e30665767a53886371a08393e9
rezonmain <adiazgomez101@gmail.com>
2022-08-24 00:21:33 -0700

    testing cors for fetching minecraft texture

--- web/components/SkinViewer/SkinViewer.tsx
+++ web/components/SkinViewer/SkinViewer.tsx
@@ -1,22 +1,24 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SkinViewer } from 'skinview3d';
 
 const Viewer = ({ type, skin }: { type: 'classic' | 'slim'; skin: string }) => {
+	const [render, setRender] = useState(false);
 	const ref = useRef<HTMLCanvasElement>(null);
 	useEffect(() => {
-		const img = new Image(64, 64);
-		img.src = skin;
-		let sv = new SkinViewer({
-			canvas: ref.current as HTMLCanvasElement,
-			width: 300,
-			height: 400,
-			skin,
-		});
-
-		sv.background = 0x5a76f3;
-	});
+		fetch(skin)
+			.then((res) => res.blob())
+			.then((blob) => URL.createObjectURL(blob))
+			.then((url) => {
+				let skinViewer = new SkinViewer({
+					canvas: ref.current as HTMLCanvasElement,
+					width: 300,
+					height: 400,
+					skin: url,
+				});
+			});
+	}, []);
 	return (
-		<div id='canvas-container' className='w-full h-full'>
+		<div id='canvas-container' className='w-full h-full bg-white'>
 			<canvas ref={ref} id='skin-viewer'></canvas>
 		</div>
 	);
