92a8a58cb1c9ae2da70eeb8ef291774e8832251c
Roberto <adiazgomez101@gmail.com>
2022-08-27 21:59:49 -0700

    STAGGING????

--- src/dramatiq_actors.py
+++ src/dramatiq_actors.py
@@ -20,7 +20,6 @@ def slp(ip, count, total):
   try:
     status = StatusPing(ip)
     res = status.get_status()
-    worker_log.send(f'[{count}/{total}] {Color.GREEN}Succesfully{Color.END} pinged {Color.YELLOW}{ip}{Color.END}, staged to save to db.', __name__)
     # Generate entry object
     ts = int(time() * 1000)
     entry = {
@@ -31,6 +30,7 @@ def slp(ip, count, total):
     # Stage entry to redis store
     worker_log(f'CACHING????{entry}')
     cache.stage(entry)
+    worker_log.send(f'[{count}/{total}] {Color.GREEN}Succesfully{Color.END} pinged {Color.YELLOW}{ip}{Color.END}, staged to save to db.', __name__)
   except:
     worker_log.send(f'[{count}/{total}] {Color.RED}{ip}{Color.END} is not a minecraft server I guess', __name__)
 
